fdb80ff359926f8b6391dd976438705b99fc3d69
lvxuan96 <1456870988@qq.com>
2020-08-26 20:45:21 +0800

    add parameter -threads

--- test/run_caller.sh
+++ test/run_caller.sh
@@ -12,7 +12,7 @@ do
 
     starttime=`date +'%Y-%m-%d %H:%M:%S'`
     # data preprocessing
-    python path/to/generate_dataset/inference_data.py -fast5 ${fast5_path} -records_dir ${tmp_records_dir} -raw_len ${signal_window_length}
+    python path/to/generate_dataset/inference_data.py -fast5 ${fast5_path} -records_dir ${tmp_records_dir} -raw_len ${signal_window_length} -threads 40
     printf "${p} preprocessing done"
 
     # caller
